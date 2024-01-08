03f1f1506ac88d87463f4364a1e87c3e93dc18b2
Wim den Braven <wimdenbraven@persuavis.com>
2024-01-08 14:54:55 -0800

    Initial commit

---
+++ lib/bp3/string.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative 'string/version'
+
+module Bp3
+  module String
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/bp3/string/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Bp3
+  module String
+    VERSION = '0.1.0'
+  end
+end
